3e9a720f395dfac57850d8424ddfe2b2a376944d
Artem <temaspsxxx@gmail.com>
2020-10-01 22:12:51 +0300

    feat: add action dispatch

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 import { BookListItem } from '../book-list-item'
 import { connect } from 'react-redux'
+import { withBookstoreService } from '../hoc'
+import { booksLoaded } from '../../actions'
+import { compose } from '../../utils'
+
 import './book-list.css'
 
 class BookList extends Component {
+
+  componentDidMount() {
+    // receive data
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks()
+
+    // dispatch action to store
+    this.props.booksLoaded(data)
+  }
+
   render() {
     const { books } = this.props;
     return (
@@ -22,4 +36,11 @@ class BookList extends Component {
 
 const mapStateToProps = ({ books }) => ({ books })
 
-export default connect(mapStateToProps)(BookList)
+const mapDispatchToProps = {
+  booksLoaded
+}
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList)
