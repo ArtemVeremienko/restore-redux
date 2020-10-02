73187e97c5c0182358a1e872107162312917fa6d
Artem <temaspsxxx@gmail.com>
2020-10-02 19:18:51 +0300

    feat: add price and image url to service

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -6,11 +6,15 @@ export default class BookstoreService {
         id: 1,
         title: 'You Dont Know JavaScript Yet: Get Started',
         author: 'Kyle Simpson',
+        price: 18.95,
+        coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-get-started/hero?1580587360',
       },
       {
         id: 2,
         title: 'YDKJS yet: Scope & Closures',
         author: 'Kyle Simpson',
+        price: 23.95,
+        coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-scope-closures/hero?1583299851'
       },
     ]
   }
