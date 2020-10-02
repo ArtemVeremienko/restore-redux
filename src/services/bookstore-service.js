344847c0d61cdbf1282eb729da3dd350f5952dfd
Artem <temaspsxxx@gmail.com>
2020-10-02 23:52:24 +0300

    feat: add async simulation to service

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,21 +1,26 @@
 export default class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: 'You Dont Know JavaScript Yet: Get Started',
+      author: 'Kyle Simpson',
+      price: 18.95,
+      coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-get-started/hero?1580587360',
+    },
+    {
+      id: 2,
+      title: 'YDKJS yet: Scope & Closures',
+      author: 'Kyle Simpson',
+      price: 23.95,
+      coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-scope-closures/hero?1583299851'
+    },
+  ]
 
   getBooks() {
-    return [
-      {
-        id: 1,
-        title: 'You Dont Know JavaScript Yet: Get Started',
-        author: 'Kyle Simpson',
-        price: 18.95,
-        coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-get-started/hero?1580587360',
-      },
-      {
-        id: 2,
-        title: 'YDKJS yet: Scope & Closures',
-        author: 'Kyle Simpson',
-        price: 23.95,
-        coverImage: 'https://d2sofvawe08yqg.cloudfront.net/ydkjsy-scope-closures/hero?1583299851'
-      },
-    ]
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.data)
+      }, 700)
+    })
   }
 }
