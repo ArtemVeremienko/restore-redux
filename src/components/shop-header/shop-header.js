652816b28b477a4ec6d219f4ed0934a47673d30a
Artem <temaspsxxx@gmail.com>
2020-10-02 23:54:53 +0300

    refactor: change a to link router

--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './shop-header.css'
 
 export const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
-      <a className="logo text-dark" href="#">ReStore</a>
-      <a className="shopping-cart">
-        <span className="cart-icon fa fa-shopping-cart" />
-        {numItems} items (${total})
-      </a>
-    </header>
+      <Link to="/">
+        <h2 className="logo text-dark" href="#">ReStore</h2>
+      </Link>
+      <Link to="/cart">
+        <p className="shopping-cart">
+          <span className="cart-icon fa fa-shopping-cart" />
+          {numItems} items (${total})
+      </p>
+      </Link>
+    </header >
   )
 }
