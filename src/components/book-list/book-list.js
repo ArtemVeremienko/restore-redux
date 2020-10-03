2914ffaf537ce301c8123ce1524d49910458c929
Artem <temaspsxxx@gmail.com>
2020-10-03 22:48:56 +0300

    feat: add component container

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -9,7 +9,21 @@ import { ErrorIndicator } from '../error-indicator'
 
 import './book-list.css'
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map((book) => {
+          return (
+            <li key={book.id}><BookListItem book={book} /></li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+class BookListContainer extends Component {
 
   componentDidMount() {
     this.props.fetchBooks()
@@ -22,19 +36,10 @@ class BookList extends Component {
 
     if (error) return <ErrorIndicator />
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}><BookListItem book={book} /></li>
-            )
-          })
-        }
-      </ul>
-    )
+    return <BookList books={books} />
   }
 }
+
 
 const mapStateToProps = ({ books, loading, error }) => ({ books, loading, error })
 
@@ -47,4 +52,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList)
+)(BookListContainer)
