8ef477106d515a8f52f1d96111d4e77ba19d6742
Artem <temaspsxxx@gmail.com>
2020-10-05 11:56:26 +0300

    feat: add fancy spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -2,5 +2,9 @@ import React from 'react'
 import './spinner.css'
 
 export const Spinner = () => {
-  return <div>loading...</div>
+  return (
+    <div className="spinner spinner-border text-primary" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
 }
